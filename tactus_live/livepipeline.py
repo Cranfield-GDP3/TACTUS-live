407a4660cd556807e2decc633e82d61318f0f1a3
Alcharyx <104650995+Alcharyx@users.noreply.github.com>
2023-04-17 10:52:59 +0000

    feat & build : Visualisation with openCV
     create new livepipeline.py file to get the previous main function as init_camera_live_pipeline
     change main to the core of the pipeline that will handle all the livepipeline (WIP)

---
+++ tactus_live/livepipeline.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+from tactus_yolov7 import Yolov7, resize
+from deep_sort_realtime.deepsort_tracker import DeepSort
+from tactus_data import skeletonization
+from tactus_model.utils.tracker import FeatureTracker, PredTracker
+from tactus_model.utils.classifier import Classifier
+from tactus_live.stream import Stream, get_rtsp
+from tactus_data.utils.visualisation import cv2_plot, plot_predict, plot_bbx
+from kafka_producer import kafka_producer
+import cv2
+
+AVAILABLE_CLASSES = ['kicking', 'punching', 'pushing', 'neutral']
+
+
+def init_camera_live_pipeline(model_path: Path, rstp_url: str,
+                              computing_device: str = "cuda:0", flag_save: bool = False):
+    """
+
+    """
+    # Init models
+    model_yolov7 = Yolov7(skeletonization.MODEL_WEIGHTS_PATH, computing_device)
+    deepsort = DeepSort(n_init=3, max_age=0)
+    feature_tracker = FeatureTracker(deepsort, window_size=9, angles_to_compute=[])
+    prediction_tracker = PredTracker()
+    classifier = Classifier()
+    classifier = classifier.load(model_path)
+    # Init Kafka
+    producer_ip = 'x'
+    topic_name = 'x'
+    # producer = kafka_producer(producer_ip,topic_name) #add get kafka data function from secret file
+
+    # Message parameters
+    EventID = 1
+    SensorID = "Camera1"
+    Priority = 1
+    VirtualInterCoord = "Not Defined"  # Position on screen
+    XPos = "not Defined"  # Lat
+    YPos = "not Defined"  # Long
+    print("Start Stream")
+    bbx = []
+    first = True
+
+    stream = Stream(rstp_url, target_fps=10) # change direct rstp to a function loading rstp data from secret file
+    if not stream.isOpened():
+        print("Error, cannot read stream")
+        exit()
+    while stream.isOpened():
+        ret, frame = stream.read()
+        if ret is True:
+            img = cv2.resize(frame, (0, 0), fx=0.33, fy=0.33)
+            img = resize(img)
+            if flag_save and first:
+                writer = cv2.VideoWriter_fourcc(*'XVID')
+                output = cv2.VideoWriter('video_output.avi', writer, 10, (img.shape[1], img.shape[0]))
+                first = False
+            skeletons = model_yolov7.predict_frame(img)
+            if len(skeletons) == 0:
+                continue
+            feature_tracker.track_skeletons(skeletons, img)
+            print(feature_tracker.rolling_windows)
+            count = 0
+            for track_id, (success, features) in feature_tracker.extract():
+                if track_id not in prediction_tracker:
+                    prediction_tracker.init_track_id(track_id)
+                if not success:
+                    cv2_plot(img, skeletons[count],prediction_tracker[track_id])
+                    continue
+                prediction = classifier.predict([features])
+                if prediction[0] != 3:
+                    # producer.set_json_message(EventID,
+                    #                          prediction,
+                    #                          f'Event : {prediction} happened at Camera n°{SensorID}',
+                    #                          SensorID,
+                    #                          Priority,
+                    #                          VirtualInterCoord ,
+                    #                          XPos,
+                    #                          YPos)
+                    # producer.poll() # Flush pending msg
+                    # producer.produce()
+                    # producer.flush() # Flush msg
+                    # time.sleep(0.1)
+                    EventID += 1
+                prediction_tracker.add_pred(track_id, AVAILABLE_CLASSES[prediction[0]], skeletons[count]["box"])
+                cv2_plot(img, skeletons[count], prediction_tracker[track_id],track_id)
+                count += 1
+                print(track_id, prediction)
+            if flag_save:
+                output.write(img)
+            cv2.imshow('Dartec Camera', img)
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+    stream.release()
+    if flag_save:
+        output.release()
+    cv2.destroyAllWindows()
+
--- tactus_live/main.py
+++ tactus_live/main.py
@@ -1,93 +1,9 @@
-import time
 from pathlib import Path
+from livepipeline import init_camera_live_pipeline
 
-import matplotlib.pyplot as plt
-from tactus_yolov7 import Yolov7, resize
-from deep_sort_realtime.deepsort_tracker import DeepSort
-from tactus_data import skeletonization
-from tactus_model.utils.tracker import FeatureTracker
-from tactus_model.utils.classifier import Classifier
-from tactus_live.stream import Stream,get_rtsp
-from tactus_data.utils.visualisation import pipeline_visualisation, plot_predict
-from kafka_producer import kafka_producer
-import cv2
+model_path = Path("TACTUS-live\\tactus_live\data\model\pickle.json")
+rstp_url = ".\Video_for_hilda\\groundPOV.MP4"
+computing_device = "cuda:0"
+flag_save = False
 
-
-
-
-
-def main():
-    computing_device = "cuda:0"
-    model_yolov7 = Yolov7(skeletonization.MODEL_WEIGHTS_PATH, computing_device)
-    deepsort = DeepSort(n_init=3, max_age=0)
-    feature_tracker = FeatureTracker(deepsort, window_size=9, angles_to_compute=[])
-    classifier = Classifier()
-    classifier = classifier.load(Path("D:\Documents\Cranfield\GDP\TACTUS-live\\tactus_live\data\model\pickle.json"))
-    rtsp_url = get_rtsp('x','x','x','x')
-    producer_ip = 'x'
-    topic_name = 'x'
-    #producer = kafka_producer(producer_ip,topic_name)
-
-    # Message parameters
-    EventID = 1
-    SensorID = "Camera3"
-    Priority = 1
-    VirtualInterCoord = "Not Defined" # Position on screen
-    XPos = "not Defined" # Lat
-    YPos = "not Defined" # Long
-    print("Start Stream")
-    bbx = []
-    stream = Stream("D:\Documents\Cranfield\GDP\Video_for_hilda\\GroundPOV.MP4",target_fps=10)
-    fig,ax = plt.subplots()
-    if not stream.isOpened():
-        print("Error, cannot read stream")
-        exit()
-    while stream.isOpened():
-        ret, frame = stream.read()
-        if ret is True:
-
-
-            img = cv2.resize(frame,(0,0),fx= 0.5, fy = 0.5)
-            img = resize(img)
-            skeletons = model_yolov7.predict_frame(img)
-            if len(skeletons) == 0:
-                continue
-
-            feature_tracker.track_skeletons(skeletons,img)
-            for i in skeletons:
-                pipeline_visualisation(ax, i["keypoints"])
-            print(feature_tracker.rolling_windows)
-            for skeleton_id, (success, features) in feature_tracker.extract():
-                if not success:
-                    continue
-                prediction = classifier.predict([features])
-                plot_predict(ax,prediction[0],skeletons[int(skeleton_id)-1]["keypoints"])
-                print(skeleton_id,prediction)
-
-            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            ax.imshow(img_rgb)
-            plt.pause(0.001)
-            ax.clear()
-
-            #preprocessing = preprocessing(skeleton)
-            #prediction = predict(preprocessing)
-            prediction =1
-            #producer.set_json_message(EventID,
-            #                          prediction,
-            #                          f'Event : {prediction} happened at Camera n°{SensorID}',
-            #                          SensorID,
-            #                          Priority,
-            #                          VirtualInterCoord ,
-            #                          XPos,
-            #                          YPos)
-            #producer.poll() # Flush pending msg
-            #producer.produce()
-            #producer.flush() # Flush msg
-            EventID +=1
-            time.sleep(0.1)
-
-
-    stream.release()
-
-
-main()
+init_camera_live_pipeline(model_path, rstp_url, computing_device, flag_save)

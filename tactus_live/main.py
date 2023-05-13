36c00e4dd1b33c71030b9fe806a14f62727615e0
Marc Bresson <marco.bresson@gmail.com>
2023-05-13 20:05:45 +0100

    feat: add a working pipeline

--- tactus_live/main.py
+++ tactus_live/main.py
@@ -1,6 +1,8 @@
 import copy
 from typing import List
 from pathlib import Path
+from cProfile import Profile
+import tqdm
 
 import cv2
 from tactus_data import PosePredictionYolov8, VideoCapture
@@ -11,21 +13,22 @@ from deep_sort_realtime.deep_sort.track import Track
 
 
 def main(device: str = "cuda:0"):
-    # Must return a list of Skeleton with at least their bounding box
-    deepsort_tracker = DeepSort(n_init=5, max_age=10)
-    # Must return a list of Skeleton
-    pose_model = PosePredictionYolov8(Path("data/raw/models"), "yolov8s-pose.pt", device)
+    deepsort_tracker = DeepSort(n_init=5, max_age=10, device=device)
+    pose_model = PosePredictionYolov8(Path("data/raw/models"), "yolov8m-pose.pt", device)
 
-    classifier = Classifier.load(Path("data/models/22.pickle"))
+    classifier = Classifier.load(Path("data/models/BEST_RUS.pickle"))
     feature_tracker = FeatureTracker(classifier.window_size, classifier.angle_to_compute)
     pred_tracker = PredTracker()
 
-    cap = VideoCapture(r"C:\Users\marco\Downloads\img_7350 low bitrate.mp4", target_fps=10)
+    cap = VideoCapture(r"C:\Users\marco\Downloads\img_7350 low bitrate.mp4",
+                       target_fps=10,
+                       tqdm_progressbar=tqdm.tqdm())
 
+    pr1 = Profile()
+    pr1.enable()
     try:
-        while (cap_frame := cap.read()) is not None:
+        while (cap_frame := cap.read())[1] is not None:
             frame_id, frame = cap_frame
-            frame = cv2.resize(frame, dsize=(0, 0), fx=0.5, fy=0.5)
 
             skeletons = pose_model.predict(frame)
 
@@ -37,9 +40,6 @@ def main(device: str = "cuda:0"):
                 pred_tracker.delete_track_id(track_id)
 
             for track in tracks:
-                if not track.is_confirmed():
-                    continue
-
                 # if there is no skeleton update, don't classify the action
                 if track.time_since_update > 0:
                     x_left, y_top, x_right, y_bottom = track.to_ltrb()
@@ -51,37 +51,45 @@ def main(device: str = "cuda:0"):
 
                 feature_tracker.update_rolling_window(track.track_id, skeleton)
 
+                # do not run the action classifier if the track is not confirmed
+                if not track.is_confirmed():
+                    continue
+
                 success, features = feature_tracker[track.track_id].get_features()
 
                 if success:
                     prediction = classifier.predict([features])
 
-                    if prediction != "neutral":
+                    if prediction != 3:
                         skeleton = feature_tracker.rolling_windows[track.track_id].skeleton
                         pred_tracker.add_pred(track.track_id, prediction, skeleton)
 
             for track_id in feature_tracker.rolling_windows:
                 skeleton = feature_tracker[track_id].skeleton
+                label = ""
 
                 color = (0, 255, 0)  # blue
                 # if the person was violent, track it with red
                 if track_id in pred_tracker:
                     color = (0, 0, 255)  # red
+                    label = pred_tracker[track_id]["current_label"]
 
                 if feature_tracker[track_id].is_duplicated():
                     thickness = 1
                 else:
                     thickness = 2
 
-                frame = visualisation.plot_bbox(frame, skeleton, color=color, thickness=thickness, label=track_id)
+                frame = visualisation.plot_bbox(frame, skeleton, color=color, thickness=thickness, label=f"{track_id}: {label}")
                 if not feature_tracker[track_id].is_duplicated():
-                    frame = visualisation.plot_joints(frame, skeleton, thickness=1)
+                    frame = visualisation.plot_joints(frame, skeleton, thickness=2)
 
             save_dir = Path("data/visualisation")
             save_path = save_dir / (str(frame_id) + ".jpg")
             cv2.imwrite(str(save_path), frame)
 
     finally:
+        pr1.disable()
+        pr1.print_stats('cumtime')
         cap.release()
 
 
